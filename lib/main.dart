a27802ec063ba12ffc77e6625bd30484ec7aea38
Aylton-Sitefane <aylton.sitefane1@gmail.com>
2025-05-20 10:22:23 +0300

    Substituicao de provider por flutter bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:todoapp/bloc/task_bloc.dart';
 import 'package:todoapp/views/home/myhome_page.dart';
-import 'package:todoapp/views/home/provider/task_provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
+    return MultiBlocProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (context) => TaskProvider(),
+        BlocProvider<TaskBloc>(
+          create: (context) => TaskBloc(),
         ),
+       //Adicione outros providers caso necessario
       ],
+      
       child: MaterialApp(
         title: 'Todo app',
         debugShowCheckedModeBanner: false,
